9514cdcfc6ab456e3ed7043d036dd0fdb50f804b
anakmasri <73363586+anakmasri@users.noreply.github.com>
2020-11-01 17:15:22 +0800

    Update 3.5.c

--- 3.5.c
+++ 3.5.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -8,66 +9,72 @@
 
 int main() {
 
-	void sigint_handler(int sig);
+        void sigint_handler(int sig);
 
-    	int num, i, flag = 0;
-	int check, fd[2];
+        int i;
+        int check;
+
+        int fd[2];
+        pipe(fd);
+        pid_t pid = fork();
+
+        //init signal
+        if(signal(SIGINT, sigint_handler) == SIG_ERR){
+                perror("signal");
+                exit(1);
+        }
 
 
-	//init signal
-	if(signal(SIGINT, sigint_handler) == SIG_ERR){
-		perror("signal");
-		exit(1);
-	}
+        if(pid > 0){
+                close(0);
+                close(fd[1]);
+                dup(fd[0]);
 
-	//loop
-	do{
-	//prompt user
-	printf("\nDo you want to check prime numbers? Select one below.\n");
-	printf("[1] Yes, I want to check prime numbers.\n");
-	printf("[2] No, I vented.\n");
-	printf("Please make a selection : ");
-	scanf("%d", &check);
+                int numP, flag = 0;
+                read(fd[0], &numP, sizeof(numP));
+                printf("Checking the number...\n");
+                wait(NULL);
 
-	
-		//user input
-		if (check == 1){
-			printf("\nEnter a positive integer : ");
-    			scanf("%d", &num);
-			printf("Checking the number...\n");
-		}
-		else if (check == 2){
-			exit(EXIT_SUCCESS);
-		}
+                for(i = 2; i <= numP/2; i++){
+                        //condition for non-prime
+                        if(numP % i == 0){
+                                flag == 1;
+                                break;
+                        }
+                }
+                if(numP == 1)
+                        printf("--> 1 is neither prime nor composite.\n\n");
+                else{
+                        if(flag == 0)
+                                printf("--> %d is a prime number.\n\n", numP);
+                        else
+                                printf("--> %d is not a prime number.\n\n", numP);
+                }
+        }
+        else if(pid == 0){
+                int num;
+                //prompt user
+                printf("\nDo you want to check prime numbers? Select one below.\n");
+                printf("[1] Yes, I want to check prime numbers.\n");
+                printf("[2] Nope.\n");
+                printf("Please make a selection : ");
+                scanf("%d", &check);
+                if(check == 1){
+                        printf("\nPlease enter positive integer : ");
+                        scanf("%d", &num);
+                        write(fd[1], &num, sizeof(num));
 
-		//processing
-    		for (i = 2; i <= num / 2; ++i) {
-        		// condition for non-prime
-        		if (num % i == 0) {
-            			flag = 1;
-            			break;
-        		}
-    		}
+                        exit(EXIT_SUCCESS);
+                }
+                else if(check == 2)
+                        exit(EXIT_SUCCESS);
+        }
 
-    		if (num == 1)
-       			printf("--> 1 is neither prime nor composite.\n\n");
-
-    		else {
-        		if (flag == 0)
-            			printf("--> %d is a prime number.\n\n", num);
-        		else
-           			printf("--> %d is not a prime number.\n\n", num);
-    		}
-
-		printf("Check another numbers? [1-yes|2-no] : ");
-		scanf("%d",&check);
-	}while(check == 1);
-
-	sleep(WAIT_INTERVAL);
-    	return EXIT_SUCCESS;
-
+        sleep(WAIT_INTERVAL);
+        return EXIT_SUCCESS;
 }
 
 void sigint_handler(int sig){
-	printf("\n--Ending all process--\n");	
+        printf("\n--Ending all process--\n");
 }
+
